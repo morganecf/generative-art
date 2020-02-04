c0e33d51b7177797e2245d46e33d1de9dd8b971c
Morgane Ciot <morganeciot@gmail.com>
2020-02-03 19:41:13 -0500

    Fixed a few things

--- hangouts/scraper/scrape.js
+++ hangouts/scraper/scrape.js
@@ -1,42 +1,17 @@
-const fs = require('fs');
-const puppeteer = require('puppeteer');
+// LIMIT = 655;
+// PAGE_LIMIT = 100;
+LIMIT = 20;
+PAGE_LIMIT = 5;
+TIMEOUT = 8000;
 
-const args = process.argv;
-
-LIMIT = 655;
-PAGE_LIMIT = 100;
-
-(async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-
-    // Navigate
-    await page.goto('https://mail.google.com');
-    await page.waitForSelector('input[type="email"]')
-    
-    // Enter email
-    await page.type('input[type="email"]', args[2])
-    await page.click('#identifierNext')
-
-    // Enter password
-    await page.waitForSelector('input[type="password"]', { visible: true })
-    await page.type('input[type="password"]', args[3])
-    await page.waitForSelector('#passwordNext', { visible: true })
-    await page.click('#passwordNext')
-    browser.pause();
-
-    // Search for in:chat <email> (don't use from: so that all messages are formatted the same)
-    // args[4]
-
-    // await browser.close();
-})();
-
-
+function sleep(ms) {
+    return new Promise(response => setTimeout(response, ms));
+}
 function parseTimestamp(row) {
     return row.querySelector('.gH .g3').title;
 }
 function parseName(row) {
-    return row.querySelector('.gF').innerText;
+    return row.querySelector('.gF').textContent;
 }
 function parseContent(row) {
     const el = row.querySelector('.a3s');
@@ -48,11 +23,8 @@ function parseRow(row) {
     const content = parseContent(row);
     return { name, timestamp, content };
 }
-function expand() {
-    return document.querySelector('table .adx').click();
-}
 function scrapeThread(messages) {
-    const rows = document.querySelectorAll('.gs');
+    const rows = document.querySelectorAll('.G3.G2');
     for (let i = 0; i < rows.length; i++) {
         try {
             const parsed = parseRow(rows[i]);
@@ -62,40 +34,49 @@ function scrapeThread(messages) {
         }
     }
 }
-function clickNext() {
+async function expand() {
+    // TODO might need to just click this and get content in the other format
+    const compressed = document.querySelector('table .adx');
+    if (compressed) {
+        // NOTE: document.querySelector('.nH .nH .nH.bkL .no .AO table .Bu .nH .nH .hj .ade') gets span 
+        const expandButton = document.querySelector('.T-I.J-J5-Ji.T-I-JN.L3.T-I-Zf-aw2');
+        expandButton.click();
+        await sleep(TIMEOUT);
+    }
+}
+async function clickNext() {
     const nextButton = document.querySelector('.T-I.J-J5-Ji.adg.T-I-awG.T-I-ax7.T-I-Js-Gs.L3');
     nextButton.click();
+    await sleep(TIMEOUT)
 }
-function save(filename, data, index) {
+function save(messages, index) {
+    const start = index - PAGE_LIMIT;
+    const filename = `messages_${start}-${index}.json`;
+    const data = JSON.stringify({ messages }, undefined, 4);
     const blob = new Blob([data], {type: 'text/json'});
     const event = document.createEvent('MouseEvents');
     const link = document.createElement('a');
     link.download = filename;
     link.href = window.URL.createObjectURL(blob);
-    link.dataset.downloadurl =  ['text/json', a.download, a.href].join(':');
+    link.dataset.downloadurl =  ['text/json', link.download, link.href].join(':');
     event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    link.dispatchEvent(e);
+    link.dispatchEvent(event);
 }
-function scrapeThreads(messages, index) {
-    if (index == PAGE_LIMIT) {
+async function scrapeThreads(messages, index) {
+    if (index > 0 && index % PAGE_LIMIT == 0) {
         console.log('********** SAVING **********');
-        const start = index - PAGE_LIMIT;
-        const filename = `messages_${start}-${index}`;
-        const data = JSON.stringify({ messages }, undefined, 4);
-        save(filename, data, index);
+        save(messages, index);
         messages = [];
-    }
-    if (index == LIMIT) {
+    } else if (index > LIMIT) {
         console.log('********** FINISHED **********');
+        save(messages, index);
         return;
     }
-    expand();
+    await expand();
     scrapeThread(messages);
     console.log(`Scraped thread ${index}: ${messages.length} messages total`);
-    clickNext();
-    setTimeout(() => {
-        scrapeThreads(messages, index + 1);
-    }, 3000)
+    await clickNext();
+    scrapeThreads(messages, index + 1);
 }
 
 messages = [];
